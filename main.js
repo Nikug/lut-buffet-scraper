082dc169d5ff99379b3a51d22152f35fd2c60cfd
Niku <n@pop-os.localdomain>
2020-09-02 17:00:31 +0300

    parse everything except different weekdays

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
 const lutBuffetUrl = "http://www.kampusravintolat.fi/fi/lutbuffet";
+const dayNames = [
+    "maanantai",
+    "tiistai",
+    "keskiviikko",
+    "torstai",
+    "perjantai",
+    "lauantai",
+    "sunnuntai"
+]
+const restaurantName = "LUT Buffet";
+const columnStructure = ["foodName", "studentPrice", "studentWithoutKelaPrice", "staffPrice", "price"]
+const foodNames = ["Kasviskeitto", "Kasvisruoka", "Halpis", "Kallis", "Kevyesti", "Kevyesti + keitto"];
+
 
 const main = async () => {
     const body = await fetchSite(lutBuffetUrl);
@@ -25,18 +38,66 @@ const fetchSite = async (url) => {
 const parseBody = (body) => {
     const $ = cheerio.load(body);
     let foods = {};
-    const tds = $("tbody td");
+    const trs = $("tbody tr");
     
-    for(let i = 0, count = tds.length; i < count; i++) {
-        const td = tds[i];
-        const text = $(td).text().trim();
-        if(text) {
-            console.log(text);
+    let menu = {
+        name: restaurantName,
+        categories: []
+    };
+
+    let currentDay = {};
+    let foodNameIterator = 0;
+    for(let row = 0, rows = trs.length; row < rows; row++) {
+        const tds = $("td", trs[row]);
+
+        let category = { foods: [] };
+
+        for(let col = 0, cols = tds.length; col < cols; col++) {
+            const td = tds[col];
+            const text = $(td).text().trim();
+            if(!text) break;
+
+            if(dayNames.includes(text.toLowerCase())) {
+                console.log(text);
+                currentDay = { day: text };
+                break;
+            }
+
+
+            // add time
+            if(text.includes("klo") && currentDay.day) {
+                const availability = text.match(/\d+.\d+\s*.\s*\d+.\d+/g);
+                if(availability.length > 0) {
+                    currentDay.availability = availability[0].replace(/\s/g, "");
+                }
+                break;
+            }
+
+            // add food and prices
+            if(currentDay.day && currentDay.availability) {
+                category.availability = currentDay.availability;
+                if(col === 0) {
+                    let food = {
+                        name: text.replace(/\s+[G|VL|VE|L|M|\*]+,*/g, ""),
+                        dietInfo: text.match(/[G|VL|VE|L|M|\*]+/g) || []
+                    }
+                    category.foods.push(food);
+                }
+                else {
+                    category[columnStructure[col]] = text + "€";
+                }
+            }
         }
-        else {
-            //console.log("empty");
+
+        if(category.foods.length > 0) {
+            category.category = foodNames[foodNameIterator++];
+            if(foodNameIterator >= foodNames.length) foodNameIterator = 0;
+            
+            category.special = null;
+            menu.categories.push(category);
         }
     }
+    console.log(JSON.stringify(menu.categories, null, 2));
 }
 
 main();
